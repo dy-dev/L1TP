70efb0feb97e09959e009db5918e185a6c69e655
arcreaneDev <dominique@arcreane.com>
2021-11-29 12:11:59 +0100

    Second exercise, with dynamic number of refunds in a separate function

--- src/com/arcreane/Main.java
+++ src/com/arcreane/Main.java
@@ -1,18 +1,56 @@
 package com.arcreane;
 
+import java.util.Scanner;
+
 public class Main {
 
     //Exercice 1 : Calcul des remises
     public static void main(String[] args) {
-	    float price = 10;
+        calculateHardRefund();
+        calculateDynamicRefund();
+    }
+
+    private static void calculateDynamicRefund() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the original price");
+        float price = scanner.nextFloat();
+
+        System.out.println("How many refunds do you want to apply");
+        int nbRefunds = scanner.nextInt();
+
+        float[] refunds = new float[nbRefunds];
+        System.out.println("Enter your values, all in one line separated by space or one after another" +
+                "\nPlease give them in descending order");
+        for (int i = 0; i < nbRefunds; i++) {
+            refunds[i] = scanner.nextFloat();
+        }
+
+        calculateRefunds(price, refunds);
+    }
+
+    private static void calculateRefunds(float price, float[] refunds) {
+        float finalPrice = price;
+        //Check if all refunds are in decreasing order
+        for (int i = 1; i < refunds.length; i++) {
+            if(refunds[i-1] < refunds[i] ){
+                System.out.println("Error in refunds order, cannot calculate final price (must be in descending order)");
+                return;
+            }
+        }
+
+        for (int i = 0; i <refunds.length; i++) {
+            finalPrice = finalPrice - finalPrice*refunds[i];
+        }
+        System.out.println("The final price is : " + finalPrice);
+    }
+
+    private static void calculateHardRefund() {
+        float price = 10;
         float refund1 = 0.5f;
         float refund2 = 0.3f;
 
-        calculateRefund(price, refund1, refund2);
-    }
-
-    private static void calculateRefund(float price, float refund1, float refund2) {
         float priceFirstRefund = (price - (price * refund1));
         float priceSecondRefund = (priceFirstRefund - (priceFirstRefund* refund2));
+        System.out.println(priceSecondRefund);
     }
 }
